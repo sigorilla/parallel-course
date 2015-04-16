e8ebe6e1a904cf2169228245ce7942c340c55392
Igor Stepanov <sigorilla@gmail.com>
2015-04-17 00:03:20 +0300

    Input test in pthread

---
+++ pthread/harm_series.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int i, n, N, n_start, n_master, n_slave;
+int *ids;
+double sum;
+
+void *Summator( void * );
+
+int main( int argc, char **argv ) {
+
+	if ( argc != 3 ) {
+		printf( "Wrong numbers of parameters. Add n and N.\n" );
+		return -1;
+	}
+
+	n = atoi( argv[ 1 ] );
+	N = atoi( argv[ 2 ] );
+
+	n_slave = (int) (N / n);
+	n_master = n_slave + N % n;
+
+	int i = 1;
+
+	if ( !( ids = ( int * ) malloc( sizeof( int ) * n ) ) ) {
+		printf( "Error of allocating memory\n" );
+		exit( -1 );
+	}
+
+	pthread_t tids[ n ];
+
+	for ( i = 0; i < n; i++ ) {
+		ids[ i ] = i;
+		if ( pthread_create( &tids[ i ], NULL, Summator, ( void * ) &ids[ i ] ) ) {
+			printf( "Error of creating thread: %s\n", strerror( errno ) );
+			return -1;
+		}
+	}
+
+	for ( i = 0; i < n; i++ ) {
+		if ( pthread_join( tids[ i ], NULL ) ) {
+			printf( "Error of joining thread: %s\n", strerror( errno ) );
+			return -1;
+		}
+	}
+
+	printf("Sum of harmonical series 1/n = %.4lf for N = %d\n", sum, N);
+
+	return 0;
+}
+
+
+void *Summator( void *arg ) {
+	int id = *( int * ) arg, start = 1;
+
+	if ( id != 0 ) {
+		start = n_master + 1 + (id - 1) * n_slave;
+	}
+
+	int finish = n_master + 1;
+	if ( id != 0 ) {
+		finish = start + n_slave;
+	}
+
+	while ( start < finish ) {
+		pthread_mutex_lock( &mutex );
+
+		sum += 1.0 / start;
+
+		printf( "ID: %d | n = %d\n", id, start );
+
+		pthread_mutex_unlock( &mutex );
+		start++;
+	}
+
+	pthread_exit( NULL );
+}
+
---
+++ pthread/send_num.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int i, n, last = -1;
+int *ids;
+int token;
+
+void *Number( void * );
+
+int main( int argc, char **argv ) {
+
+	if ( argc != 2 ) {
+		printf( "Wrong numbers of parameters. Add n.\n" );
+		return -1;
+	}
+
+	n = atoi( argv[ 1 ] );
+
+	int i = 0;
+
+	if ( !( ids = ( int * ) malloc( sizeof( int ) * n ) ) ) {
+		printf( "Error of allocating memory\n" );
+		return -1;
+	}
+
+	pthread_t tids[ n ];
+
+	for ( i = 0; i < n; i++ ) {
+		ids[ i ] = i;
+		if ( pthread_create( &tids[ i ], NULL, Number, ( void * ) &ids[ i ] ) ) {
+			printf( "Error of creating thread: %s\n", strerror( errno ) );
+			return -1;
+		}
+	}
+
+	for ( i = 0; i < n; i++ ) {
+		if ( pthread_join( tids[ i ], NULL ) ) {
+			printf( "Error of joining thread: %s\n", strerror( errno ) );
+			exit( -1 );
+		}
+	}
+
+	return 0;
+}
+
+
+void *Number( void *arg ) {
+	int id = *( int * ) arg;
+
+	if ( id == 0 ) {
+		pthread_mutex_lock( &mutex );
+		last = id;
+		token = 123;
+		pthread_mutex_unlock( &mutex );
+	} else {
+		while (last < id - 1) {
+			continue;
+		}
+		printf("Thread %d received token %d from thread %d\n", 
+			id, token, id - 1);
+		pthread_mutex_lock( &mutex );
+		last = id;
+		token += 1;
+		pthread_mutex_unlock( &mutex );
+	}
+
+	if ( id == 0 ) {
+		while ( last < n - 1 ) {
+			continue;
+		}
+		printf("Thread %d received token %d from thread %d\n", 
+			id, token, n - 1 );
+	}
+
+	pthread_exit( NULL );
+}
